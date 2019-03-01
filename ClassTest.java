cfb664c9138072f89cb89f24115b4216b194d86c
alifaheem43 <alifaheem5089@gmail.com>
2019-03-01 17:11:52 +0500

    first commit

---
+++ ClassTest.java
@@ -0,0 +1,14 @@
+ClassTest 
+{
+	public static void main (string [] args) 
+	{
+	  int x = o;
+	  int y = o;
+	  while (x < 5 )
+	 {
+	  y = x - y;
+	  Syestem.out.print(x + "" + y + "") ;
+	  x= x + 1;
+	  }
+	}
+}
