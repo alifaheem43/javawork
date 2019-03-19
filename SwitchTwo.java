524b6ba68bf2b21ec7674ac12ae6a7a58a40093d
alifaheem43 <alifaheem5089@gmail.com>
2019-03-19 11:56:53 +0500

    first commit

---
+++ SwitchTwo.java
@@ -0,0 +1,19 @@
+class SwitchTwo
+{
+	public static void main(String args [])
+	{
+	 int i;
+	 for(i=0; i<=4; i++)
+	 {
+	  switch (i)
+	  {
+	   case 1:
+	   case 2:
+	   case 3: System.out.println("i is 1, 2 or 3");
+	   break;
+	   case 4: System.out.println("i is 4");
+	   break;
+	  }
+	 }
+	}
+}
